59b6d101247bcb5e80675d48876351f9b4147d22
Vinicius Gonzaga Rocha <viniciusgr@algartech.com>
2018-10-24 20:45:35 -0300

    adding the graphql schema

---
+++ schema.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Costumer',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: { type: GraphQLString },
+            },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/customers/${args.id}`)
+                    .then(res => res.data);
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve() {
+                return axios.get(`http://localhost:3000/customers`)
+                    .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
